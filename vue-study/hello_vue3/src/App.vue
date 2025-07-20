d267cf20d2879fed4130524f2579cbfebca42487
Ryan-Joee <3497882088@qq.com>
2025-07-20 19:40:14 +0800

    vue3学习

---
+++ vue-study/hello_vue3/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- 这里面写html -->
+  <div class="app">
+    <h1>你好呀</h1>
+  </div>   
+</template>
+
+<script lang="ts">
+  /* 这里面写js或者ts */
+  export default {
+    name: 'App' // 组件名称
+  }
+</script>
+
+<style>
+/* 这里面写css */
+  .app {
+    background-color: #ddd;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+  }
+</style>
